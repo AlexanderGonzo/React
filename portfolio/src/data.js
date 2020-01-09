3f82ee5e2c7fe819e92d90b553ef2bcedd9e6d49
AlexanderGonzo <alexgonzo1994@gmail.com>
2020-01-08 19:25:38 -0600

    started working branch, started data.js

---
+++ portfolio/src/data.js
@@ -0,0 +1,45 @@
+let data = {
+  name: "Alexander Gonzalez",
+  role: "Full Stack Developer",
+  description:
+    "I'm a Houston, Texas based Full Stack Developer focused on ReactJS and a series of other JS frameworks. Feel free to take a look at my latest projects on the web portfolio page!",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/alexander-gonzalez-5829a4171/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://www.github.com/AlexanderGonzo",
+      className: "fa fa-github"
+    }
+  ],
+  education: [
+    {
+      uniName: "Texas State University",
+      degree: "Bachelors of Science in Computer Science"
+    }
+  ],
+  work: [
+      {
+          "company": "SWBC",
+          "role": "Information Technology Intern",
+          "achievement": "Designed, documented, and implemented an end-to-end robotic process automation for 12 virtual desktop interfaces by using UiPath, resulting in an average savings of 15 full - time employee hours weekly.",
+          "duration": "September - December 2019" 
+        },
+      {
+          "company": "KTSW",
+          "role": "Web Developer",
+          "achievement": "As a lead Web Developer for a small team, managed overall development; oversaw daily projects for site maintenance and web development tasks for two domains; implemented a development tool that lead to a 25% reduction in network failure.",
+          "duration": "November 2018 - November 2019"
+      },      
+  ],
+    "skills": " Tactical technical experience and a working knowledge of Agile development and Robotic Automation Processes as well as proficiencies in C / C++, JavaScript, ReactJS, Python, Bash/ Shell Scripting, and HTML / CSS.",
+    "portfolio":[
+        {
+            "name": "Radio Live Stream Web Application",
+            "description": "A ReactJS web application that utilized custom developed micro services in order to provide the user with flawless audio and dynamically presented the currently playing song."
+        }
+    ]
+};
